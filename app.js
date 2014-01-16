6741b681a24d9bd744921b20f0c8dfceb5e1d391
andyzg <zhangandy94@gmail.com>
2014-01-16 10:24:31 -0500

    Added documentation, fixed view update

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // request handlers
 var handler = function(req, res) {
-	// console.log(req.url);
+	// path handler
 	var filePath = req.url === '/' ? "./views/index.html" : "./views" + req.url;
-	console.log(filePath);
 	fs.readFile(filePath, function(err, data) {
 		if (err) {
 			res.writeHead(500);
@@ -26,9 +25,11 @@ app.listen(port);
 
 io.sockets.on('connection', function(socket) {
 
+	// Holds name of client and states
 	var user = "";
 	var state = "Starting soon";
 	
+	// Updates the state
 	var getMessage = function() {
 		if (user === null || user === "") {
 			return "Please enter a cool name";
@@ -40,34 +41,41 @@ io.sockets.on('connection', function(socket) {
 	
 	var message = getMessage();
 	
+	// Request the user's name
 	socket.emit('promptuser', message);
 	
+	// After user submits name, updates the view
 	socket.on('ready', function(name) {
+		
+		// If name is taken, repeat process
 		if (game.isRepeat(name) || name == null) {
 			message = getMessage();
 			socket.emit('promptuser', message);
 			return;
 		}
 		
+		// Adds user and sets up the view
 		user = name;
 		game.addUser(name);
 	//	updateScreen();
 		io.sockets.emit('refreshstate', state, game.getUsers());
 	});
 	
+	// If disconnected, remove the user from activeUser
 	socket.on('disconnect', function() {
 		if (user === null){
 			return;
-		}
-		
+		}	
 		game.removeUser(user);
 	});
 	
+	// Check if they have the correct answer
 	socket.on('click', function(question, answer) {
 		var valid = game.checkAnswer(question, answer);
 		socket.emit('update', valid, user);
 	});
 	
+	// Toggles the state if in game or waiting for new game.
 	var toggleState = function () {
 		if (state === "Starting soon") {
 			return state = "In Progress";
--- views/script.js
+++ views/script.js
@@ -1,48 +1,25 @@
 window.onload = function() {
-	
 	var socket = io.connect('http://localhost');
     
+	// Prompts user for his/her name
     socket.on('promptuser', function(message) {
         var name = prompt(message, "Your Name");
         socket.emit('ready', name);
     });
 
     socket.on('refreshstate', function(state, activeUsers) {
+    	// Update the title/state of game
         var title = document.getElementById('title');
         title.innerHTML = state;
-
+        
+        
+        // Add active users to be displayed in list
         var list = document.getElementById('list');
+        list.innerHTML = "";
         for (var i=0; i<activeUsers.length; i++) {
                 var name = document.createElement("p");
                 name.innerHTML = activeUsers[i].name;
                 list.appendChild(name);
         }
     });
-/*
-var welcome = document.getElementById("welcome");
-var allUsers = document.getElementById("users");
-var progress = document.getElementById("progress");
-var results = document.getElementById("results");
-
-var socket = io.connect('http://localhost:3000');
-
-socket.on('welcome', function (data) {
-	console.log(data);
-	welcome.innerHTML = "Welcome to the game <strong>" + data.name + "</strong>";
-});
-socket.on('users', function (data) {
-	allUsers.innerHTML = "<strong>Users:</strong><br />" + data.users;
-});
-socket.on('update', function (data) {
-	progress.innerHTML = data.currentWidth;
-	progress.style.width = parseInt(data.currentWidth) + "px";
-});
-socket.on('win', function (data) {
-	results.innerHTML = data.message;
-});
-
-progress.onclick = function() {
-        socket.emit("click");
-}
-*/
-}
+};
